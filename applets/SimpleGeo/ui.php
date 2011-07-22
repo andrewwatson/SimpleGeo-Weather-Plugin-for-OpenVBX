156ff33eaa62ca7c3928d4d1ec9588120727a19e
Andrew Watson <andrewmoorewatson@gmail.com>
2011-07-22 00:23:42 -0400

    oops bug

--- applets/SimpleGeo/ui.php
+++ applets/SimpleGeo/ui.php
@@ -5,6 +5,6 @@
 	<h2>Enter city and state here and your callers will here the current conditions there.</h2>
 	<p>This is brought to you by the SimpleGeo API</p>
 	<div class="vbx-input">
-	<input name="address">
+	<input name="address" value="<?php echo $address; ?>">
 	</div>
 </div>
